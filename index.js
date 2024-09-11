6db7e0c3637fb8fef354d7e2a1d7a3b52efb3eef
Thais <thais.andrezza.g.s@gmail.com>
2024-09-11 11:42:40 -0300

    Metas Abertas

--- index.js
+++ index.js
@@ -61,11 +61,26 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas "  +  realizadas.length,
         choices: [...realizadas]
     })
 }
 
+const metasAbertas = async () =>{
+    const abertas = metas.filter ((meta)=>{
+        return meta.checked != true
+    })
+
+    if (abertas.length==0){
+        console.log("Não existem metas abertas! Parabéns!")
+        return
+    }
+
+    await select({
+        message: "Metas abertas " + abertas.length,
+        choices: [...abertas]
+    })
+}
 
 
 const start = async () => {
@@ -88,6 +103,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas Abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -106,6 +125,10 @@ const start = async () => {
 
                 case "realizadas":
                     await metasRealizadas()
+                break
+
+                case "abertas":
+                    await metasAbertas()
                 break
 
                 case "sair":
