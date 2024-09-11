c9f953bcc2e30064881d2420a2d01600659eb495
Thais <thais.andrezza.g.s@gmail.com>
2024-09-11 09:48:44 -0300

    Menu da aplicação

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-function start () {
+const { select } = require ('@inquirer/prompts')
+
+const start = async () => {
     
     while(true){
-        let opcao = "cadastrar"
-        switch(opcao){
-            case
+
+        const opcao = await select ({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar metas",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+        
+                switch(opcao) {
+                case "cadastrar":
+                    console.log ("vamor cadastrar")
+                break
+
+                case "listar":
+                    console.log ("vamos listar")
+                break
+
+                case "sair":
+                    console.log ("Até a próxima!")
+                    return
+                }
         }
         
     }
-}
+
 
 start()
